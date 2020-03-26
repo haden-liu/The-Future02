18c37b19ec9d4ccb78dc1f832cfe2585f836fe56
Cynthia Zhou <lbx01072004@gmail.com>
2020-03-26 21:31:08 +1100

    update

--- assets/Zodiac.js
+++ assets/Zodiac.js
@@ -8,6 +8,31 @@
 // returns 
 var sunSigns = "https://zodiacal.herokuapp.com/api";
 console.log(sunSigns);
+// sunSigns length=12
+    //0 Aries
+    //1 Taurus
+    //2 Gemini
+    //3 Cancer 
+    //4 Leo 
+    //5 Virgo 
+    //6 Libra 
+    //7 Scorpio 
+    //8 Saggittarius
+    //9 Capricon
+    //10 Aquarius
+    //11 Pisces
+
+// sunSigns[0] famous_people: length=32
+    // 0: "Lady Gaga"
+    // 2: " Celine Dion"
+    // 16: " Kourtney Kardashian"
+    // 17: " Big Sean"
+    // 22: " Tommy Hilfiger"
+    // 26: " Jessie J"
+    // 28: " Robert Downey Jr."
+
+// sunSigns[1] famous_people:
+
 
 // retuns rising physical and mental traits
 var risingSigns = "https://zodiacal.herokuapp.com/rising";
@@ -31,13 +56,19 @@ function displayZodiac() {
         method: "GET"
     })
         .then(function(response) {
+            console.log(response[0].rising_physical)
+            console.log(response[0].famous_people)
             console.log(response)
-            var sunResults = response.data;
+            
+            
+            var sunResults = response;
 
             for (var i = 0; i < sunResults.length; i++) {
                 var sunDiv = $("<div>");
+                $(sunResults).append(sunDiv)
             }
 
             
         });
 };
+displayZodiac()  
