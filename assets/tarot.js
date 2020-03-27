52b4944aa9a4f1509be00e945f8eade12edbfc2b
Cynthia Zhou <lbx01072004@gmail.com>
2020-03-28 10:55:32 +1100

    tarot

--- assets/tarot.js
+++ assets/tarot.js
@@ -1,6 +1,7 @@
 function displayTarot() {
 
-    var queryURL = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=20";
+
+    var queryURL = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3";
 
     $.ajax({
         url: queryURL,
